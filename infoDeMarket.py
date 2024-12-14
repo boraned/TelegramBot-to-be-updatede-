625dbee2a9bfe11b2d2d99e387e09921d253e7b4
Boran <49809633+boraned@users.noreply.github.com>
2024-12-14 03:46:01 -0800

    infoDeMarket.py

--- infoDeMarket.py
+++ infoDeMarket.py
@@ -2,36 +2,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-#Coin Market Cap
-coinmarketcap = requests.get("https://coinmarketcap.com/")
-coinmarketcapSoup = BeautifulSoup(coinmarketcap.text, "html.parser")
 
+#Coin Market Cap
 
 def CoinMarketCapInfo():
+    coinmarketcap = requests.get("https://coinmarketcap.com/")
+    coinmarketcapSoup = BeautifulSoup(coinmarketcap.text, "html.parser")
+
     coinmarketcapCoinNames = coinmarketcapSoup.findAll("p", attrs={"class" : "sc-65e7f566-0 iPbTJf coin-item-name"})
     coinmarketcapCoinPrices = coinmarketcapSoup.findAll("div", attrs={"class" : re.compile(r"sc-b3fc6b7-0 dzgUIj( rise| fall)?" )})
-
     txt = ""
     for coinName, price in zip(coinmarketcapCoinNames, coinmarketcapCoinPrices):
         txt += f"{coinName.text}:{price.text}\n"
     return txt
-
-
-#Coin Desk
-#coindesk = requests.get("https://www.coindesk.com/price")
-#coindeskSoup = BeautifulSoup(coindesk.text, "html.parser")
-#
-#def CoinDeskInfo():
-#    coindeskCoinNames = coindeskSoup.findAll("h5", attrs={"class": "Noto_Sans_sm_Sans-600-sm text-color-black "})
-#    coindeskPrices = coindeskSoup.findAll("h5", attrs={"class": "Noto_Sans_Mono_sm_Mono-400-sm text-color-black "})
-#
-#
-#    txt = ""
-#    for coinName, price in zip(coindeskCoinNames, coindeskPrices):
-#        txt += f"{coinName.text}:{price.text}\n"
-#    return txt
-
-
-def GatherAll() -> str:
-    return f"CoinMarketCap Info:\n\n{CoinMarketCapInfo()}"
-
